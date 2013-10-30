52f1bc406a174235e773826a2654e7b60ef4c7c3
al289810 <luciabluebell@hotmail.com>
2013-10-30 15:42:10 +0100

    Version JEFA

---
+++ MiPrograma.py
@@ -0,0 +1,36 @@
+# MiPrograma.py
+import random
+ 
+# -----------------------------------------------------
+# -----------------------------------------------------
+def sumarListaPares(lista):
+    sum=0
+    for i in range(0,len(lista)):
+        if lista[i] % 2 == 0:
+            sum=sum+lista[i]
+ 
+    return sum
+
+# -----------------------------------------------------
+# -----------------------------------------------------
+def imprimirLista(lista,nombre):
+    for i in range(0,len(lista)):
+        print nombre + "[" + str(i) + "]=" + str(lista[i])
+
+# -----------------------------------------------------
+# -----------------------------------------------------
+def crearLista():
+    lista=[]
+ 
+    i=0
+    while i < 5:
+        lista.append(int(random.randint(0, 5)))
+        i=i+1
+    return lista
+
+# -----------------------------------------------------
+# -----------------------------------------------------
+A = crearLista()
+imprimirLista(A,"A")
+print "Suma = " + str(sumarListaPares(A))
+
