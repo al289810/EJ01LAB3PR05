7fc9460b6a3b19b9f2c6d897840f730149d8098b
al289810 <luciabluebell@hotmail.com>
2013-10-30 16:09:14 +0100

    Version JEFA

---
+++ test.py
@@ -0,0 +1,7 @@
+import DecimalBinario
+
+numero=int(raw_input('Introduce el numero decimal a convertir a binario: '))
+print(DecimalBinario.Decimal2Binario(numero))
+
+str=raw_input('Introduce el numero binario a convertir a decimal: ')
+print(DecimalBinario.Binario2Decimal(str))
